3b0cc5a6de1374285fbb6a49606b561edb327446
Lennart Becker <Lenny@Mac-mini.fritz.box>
2020-05-07 22:29:37 +0200

    Fix Heuristic Bug; Add Save functionality

--- index.js
+++ index.js
@@ -1,124 +1,171 @@
 import { astar } from './pathfinding.js';
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
 let controlmode = 0;
 let fieldSize = 30;
 
 let mazeHeight = calculateMazeDimensions().height;
 let mazeWidth = calculateMazeDimensions().width;
+// let mazeHeight = 5;
+// let mazeWidth = 5;
+let start, end;
 
-let start = [0, 0];
-let end = [12, 12];
 let mazecontainer = document.getElementById('mazeContainer');
 
 let maze = generateMaze(mazeHeight, mazeWidth);
 let dommaze = generateDomMaze(maze);
 
 function generateMaze(height, width) {
-    let maze = [];
-    let row = new Array(width).fill(0);
-    for (let i = 0; i < height; i++) {
-        maze.push([...row]);
-    }
-    return maze;
+  let maze = [];
+  let row = new Array(width).fill(0);
+  for (let i = 0; i < height; i++) {
+    maze.push([...row]);
+  }
+  return maze;
 }
 
 function generateDomMaze(maze) {
-    let dommaze = [];
-    for (let i = 0; i < maze.length; i++) {  //Height
-        let domrow = [];
-        let rowDiv = document.createElement("div");
-        rowDiv.classList.add("row");
-        rowDiv.id = `row-${i}`
-        rowDiv.draggable = false;
-        for (let j = 0; j < maze[i].length; j++) { //Width
-            let field = document.createElement("div");
-            field.dataset.x = j;
-            field.dataset.y = i;
-            field.classList.add("field");
-            field.draggable = false;
-            field.addEventListener("mousedown", () => {
-                console.log("Clicked")
-                if (controlmode == 0) {
-                    controlmode = 1;
-                }
-                handleEdit(field)
-            })
-            field.addEventListener("mouseup", () => {
-                console.log("mouseup")
-                controlmode = 0;
-            })
-            field.addEventListener("mouseover", (ev) => {
-                if (controlmode == 1) {
-                    handleEdit(field)
-                }
-            })
-            rowDiv.appendChild(field);
-            domrow.push(field);
+  mazecontainer.innerHTML = "";
+  let dommaze = [];
+  for (let i = 0; i < maze.length; i++) {  //Height
+    let domrow = [];
+    let rowDiv = document.createElement("div");
+    rowDiv.classList.add("row");
+    rowDiv.id = `row-${i}`
+    rowDiv.draggable = false;
+    for (let j = 0; j < maze[i].length; j++) { //Width
+      let field = document.createElement("div");
+      field.dataset.x = j;
+      field.dataset.y = i;
+      // field.innerHTML = `X:${j}, Y:${i}`;
+      field.classList.add("field");
+      field.draggable = false;
+      field.addEventListener("mousedown", () => {
+        if (controlmode == 0) {
+          controlmode = 1;
         }
-        dommaze.push(domrow);
-        mazecontainer.appendChild(rowDiv);
+        handleEdit(field)
+      })
+      field.addEventListener("mouseup", () => {
+        controlmode = 0;
+      })
+      field.addEventListener("mouseover", (ev) => {
+        if (controlmode == 1) {
+          handleEdit(field)
+        }
+      })
+      rowDiv.appendChild(field);
+      domrow.push(field);
     }
-    return dommaze;
+    dommaze.push(domrow);
+    mazecontainer.appendChild(rowDiv);
+  }
+  return dommaze;
 }
 
 function handleEdit(field) {
-    let position = {
-        x: parseInt(field.dataset.x),
-        y: parseInt(field.dataset.y)
-    }
-    if (controlmode == 1) {
-        field.classList.remove("path");
+  let position = {
+    x: parseInt(field.dataset.x),
+    y: parseInt(field.dataset.y)
+  }
+  if (controlmode == 1) {
+    field.classList.remove("path");
 
-        if (maze[position.y][position.x] == 0) {
-            field.classList.add("obstacle");
-            maze[position.y][position.x] = 1;
-        } else {
-            field.classList.remove("obstacle");
-            maze[position.y][position.x] = 0;
-        }
+    if (maze[position.y][position.x] == 0) {
+      field.classList.add("obstacle");
+      maze[position.y][position.x] = 1;
+    } else {
+      field.classList.remove("obstacle");
+      maze[position.y][position.x] = 0;
     }
+  }
 
-    if (controlmode == 2) { //Set Start
-        removeClassFromAll("start");
-        field.classList.add("start");
-        start = [position.y, position.x];
-        controlmode = 0;
-    }
-    if (controlmode == 3) { //Set End
-        removeClassFromAll("end");
-        field.classList.add("end");
-        end = [position.y, position.x];
-        controlmode = 0;
-    }
+  if (controlmode == 2) { //Set Start
+    removeClassFromAll("start");
+    field.classList.add("start");
+    start = [position.y, position.x];
+    controlmode = 0;
+  }
+  if (controlmode == 3) { //Set End
+    removeClassFromAll("end");
+    field.classList.add("end");
+    end = [position.y, position.x];
+    controlmode = 0;
+  }
 }
 
 document.querySelectorAll('.mode-button').forEach(button => {
-    button.addEventListener("click", (ev) => {
-        controlmode = ev.target.dataset.mode;
-    })
+  button.addEventListener("click", (ev) => {
+    controlmode = ev.target.dataset.mode;
+  })
 })
 
-document.querySelector('#start').addEventListener('click', displayPath)
-
+document.querySelector('#start').addEventListener('click', displayPath);
+document.querySelector('#clear').addEventListener('click', clearMaze);
 function displayPath() {
+  if (start && end) {
     removeClassFromAll("path");
     let route = astar(maze, start, end);
     route.forEach(field => {
-        let y = field[0];
-        let x = field[1];
-        dommaze[y][x].classList.add("path");
+      let y = field[0];
+      let x = field[1];
+      dommaze[y][x].classList.add("path");
     })
+  } else {
+    Toastify({
+      text: "Please set the start & endpoint",
+      duration: 22000,
+      newWindow: true,
+      gravity: "top", // `top` or `bottom`
+      position: 'right', // `left`, `center` or `right`
+      backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+    }).showToast();
+  }
 }
 function removeClassFromAll(className) {
-    dommaze.forEach(row => {
-        row.forEach(field => {
-            field.classList.remove(className)
-        })
+  dommaze.forEach(row => {
+    row.forEach(field => {
+      field.classList.remove(className)
     })
+  })
+}
+
+function clearMaze() {
+  console.log("Clearmaze")
+  maze.forEach(row => {
+    row.forEach(field => {
+      field = 0;
+    })
+  })
+  removeClassFromAll("start");
+  removeClassFromAll("end");
+  removeClassFromAll("path");
+  removeClassFromAll("obstacle");
 }
 
 function calculateMazeDimensions() {
-    let height = Math.round((window.innerHeight - (window.innerHeight / 100 * 6)) / fieldSize);
-    console.log(height)
-    let width = Math.round(window.innerWidth / fieldSize);
-    return { height, width }
+  let height = Math.round((window.innerHeight - (window.innerHeight / 100 * 6)) / fieldSize);
+  console.log(height)
+  let width = Math.round(window.innerWidth / fieldSize);
+  return { height, width }
 }
+
+function saveMaze() {
+  console.log("save")
+  localStorage.setItem("maze", JSON.stringify(maze));
+  localStorage.setItem("start", JSON.stringify(start));
+  localStorage.setItem("end", JSON.stringify(end));
+}
+
+function loadMaze() {
+  console.log("Load")
+  maze = JSON.parse(localStorage.getItem("maze"));
+  start = JSON.parse(localStorage.getItem("start"))
+  end = JSON.parse(localStorage.getItem("end"))
+  dommaze = generateDomMaze(maze);
+  console.log(maze)
+}
+
+document.querySelector("#save").addEventListener("click", saveMaze);
+document.querySelector("#load").addEventListener("click", loadMaze);
--- pathfinding.js
+++ pathfinding.js
@@ -19,30 +19,34 @@ function astar(maze, start, end) {
     while (waitingList.length != 0 || foundPath == false) {
         let currentNode = waitingList.sort((a, b) => a.f > b.f)[0]; // Suche Node mit geringstem F-Wert
         if (currentNode.position[0] == end[0] && currentNode.position[1] == end[1]) {
-            console.log("End")
             foundPath = true;
             endnode = currentNode;
             break;
         }
+
         waitingList.splice(waitingList.indexOf(currentNode), 1); //Entferne es aus der warteliste
         closedList.push(currentNode); //Füge es zur Closedliste hinzu
 
         let neighbors = getNeighborPositions(currentNode, maze);
         neighbors.forEach(neighbor => { //Für jedes der Angrenzenden Quadrate
             if (closedList.find(listnode => listnode.position[0] == neighbor[0] && listnode.position[1] == neighbor[1])) {
-                // console.log("In closed list")
                 return; //Wenn nicht begehbar, oder bereits geschlossen, ignoriere es
             }
             if (maze[neighbor[0]][neighbor[1]] == 1) {
+                console.log("Hindernis")
                 return;
             }
             let neighBorNode = new MazeNode(neighbor, currentNode);
             neighBorNode.g = currentNode.g + 1;
-            neighBorNode.h = Math.pow(neighBorNode.position[0] - end[0], 2) + Math.pow(neighBorNode.position[1] - end[1], 2);
+            // neighBorNode.g = Math.pow(neighBorNode.position[0] - start[0], 2) + Math.pow(neighBorNode.position[1] - start[1], 2);
+            // neighBorNode.h = Math.pow(neighBorNode.position[0] - end[0], 2) + Math.pow(neighBorNode.position[1] - end[1], 2);
+            neighBorNode.h = Math.abs(neighBorNode.position[0] - end[0]) + Math.abs(neighBorNode.position[1] - end[1])
+            // neighBorNode.h = 0;
             neighBorNode.f = neighBorNode.g + neighBorNode.h;
 
             let inWaitinglist = waitingList.find(listnode => listnode.position[0] == neighbor[0] && listnode.position[1] == neighbor[1])
             if (inWaitinglist) {
+              // console.log("Node is in waitinglist")
                 if (inWaitinglist.g > neighBorNode.g) {
                     waitingList.splice(waitingList.indexOf(inWaitinglist), 1);
                     waitingList.push(neighBorNode);
